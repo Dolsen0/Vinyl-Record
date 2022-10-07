78e1e2d08ae7d5862143c7427421e3689d40d8fa
dolsen0 <derek.olsen@gmail.com>
2022-10-07 16:47:52 -0400

    adjusted display of app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
-import record from './images/record.svg'
-import './App.css';
+import record from "./images/record.svg";
+import "./App.css";
 
 function App() {
-
-  const options = ['Add', 'View', 'Update']
-
+  const options = ["Add", "View", "Update"];
 
   return (
     <div className="App">
       <header className="App-header">
         <h2>Vinyl Record</h2>
         <h5>Record your vinyl</h5>
-        <div className='turntable'>
-        <img src={record} className="App-logo" alt="logo" />  
+
+        <div className="container">
+          <div className="turntable">
+            <img src={record} className="App-logo" alt="logo" />
+          </div>
+          {options.map((option) => {
+            return (
+              <div
+                className="options"
+                key={option}
+                onClick={() => {
+                  console.log(`are you ready to ${option}?`);
+                }}
+              >
+                <ul>
+                  <li>
+                {option}
+                  </li>
+                </ul>
+              </div>
+            );
+          })}
         </div>
-        {options.map((option) => {
-          return(
-            <div className='options' key={option} onClick={()=>{
-              console.log(`are you ready to ${option}?`)
-            }}>
-              {option}
-            </div>
-          )
-        })}
-      
       </header>
     </div>
-
   );
 }
 
