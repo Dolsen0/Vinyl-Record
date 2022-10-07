c350c108f22a491afa23a2ce9f11e89940dddd7e
dolsen0 <derek.olsen@gmail.com>
2022-10-06 23:05:02 -0400

    added mapped options array

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import record from './images/record.svg'
 import './App.css';
 
 function App() {
+
+  const options = ['Add', 'View', 'Update']
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Vinyl Record</h1>
+        <h2>Vinyl Record</h2>
+        <h5>Record your vinyl</h5>
+        <div id='turntable'>
         <img src={record} className="App-logo" alt="logo" />  
+        </div>
+
+        {options.map((option) => {
+          return(
+            <div>
+              {option}
+            </div>
+          )
+        })}
+      
       </header>
     </div>
+
   );
 }
 
